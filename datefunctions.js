1ebec820e91245117a91d8aba375b813ffb4b3d0
G.V.S.Pujitha <pujithagvs@gmail.com>
2024-11-23 10:49:31 +0530

    all files are added

---
+++ datefunctions.js
@@ -0,0 +1,35 @@
+let a= Date()
+console.log(a)
+
+
+let b=new Date()
+console.log(b)
+console.log(b.getFullYear())
+console.log(b.getMonth())
+console.log(b.getDate())
+console.log(b.getDay())
+console.log(b.getHours())
+console.log(b.getMinutes())
+console.log(b.getSeconds())
+console.log(b.getMilliseconds())
+console.log(b.getTime())
+
+let c=new Date()
+console.log(c.setFullYear(2025))
+console.log(c.setSeconds(28))
+console.log(c.toDateString())
+console.log(c.toTimeString())
+
+
+let d=new Date()
+console.log(d.toLocaleDateString())
+console.log(d.toLocaleTimeString())
+
+
+
+
+
+
+
+
+
